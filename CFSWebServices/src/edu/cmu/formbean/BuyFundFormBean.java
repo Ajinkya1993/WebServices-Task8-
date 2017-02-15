2ddbe5576c3e823acdb04ab393cf48693e66957d
Rainabeehive <yuhanz@andrew.cmu.edu>
2017-02-14 20:53:47 -0500

    buyFundFormBean

--- CFSWebServices/src/edu/cmu/formbean/BuyFundFormBean.java
+++ CFSWebServices/src/edu/cmu/formbean/BuyFundFormBean.java
@@ -1,5 +1,62 @@
 package edu.cmu.formbean;
 
-public class BuyFundFormBean {
+import java.util.ArrayList;
+import java.util.List;
 
+public class BuyFundFormBean {
+	private String symbol;
+	private String cashValue;
+	
+	public BuyFundFormBean(String symbol, String cashValue){
+		this.symbol = symbol;
+		this.cashValue = cashValue;
+	}
+
+	public String getSymbol() {
+		return symbol;
+	}
+
+	public void setSymbol(String symbol) {
+		this.symbol = symbol;
+	}
+
+	public String getCashValue() {
+		return cashValue;
+	}
+
+	public void setCashValue(String cashValue) {
+		this.cashValue = cashValue;
+	}
+	
+	public double getCashDouble(){
+		return Double.parseDouble(cashValue);
+	}
+	public List<String> getValidationErrors() {
+		List<String> errors = new ArrayList<String>();
+		if (symbol == null || symbol.length() == 0) {
+			errors.add("The input you provided is not valid.");
+		}
+		if (cashValue == null || cashValue.length() == 0) {
+			errors.add("The input you provided is not valid");
+			
+		}
+		if (errors.size() > 0) {
+			return errors;
+		}
+		try {
+			Double.parseDouble(cashValue);
+			if (getCashDouble() < 1 || getCashDouble() > 1000000000) {
+				errors.add("The input you provided is not valid");
+				
+				
+			}
+		} catch (NumberFormatException e) {
+			errors.add("The input you provided is not valid");
+		}
+		
+		return errors;
+		
+	}
+	
+	
 }
